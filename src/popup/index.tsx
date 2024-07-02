c747428525f42aad557649e1a2e1bc9ced7b702f
takeuchi <takeuchi.hi@tsunagu-grp.jp>
2024-07-02 11:29:50 +0900

    :package: feat:add tailwind

---
+++ src/popup/index.tsx
@@ -0,0 +1,15 @@
+import '~/style.css'
+
+import { useState } from "react"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return (
+    <div className="container">
+      <h2 className="text-5xl">aa</h2>
+    </div>
+  )
+}
+
+export default IndexPopup
